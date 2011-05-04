643ba55c25a649cb7ab4a97d5818fa44d72f5197
Andrew Ngan <ngan7487@gmail.com>
2011-05-04 19:12:47 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlteregoDemo::Application.config.secret_token = 'e9d28a7f85929eb1ffa255dc4dff8f9bb2ae52f9da542d6b26ec7c55de5ce10c1fb22571e8faf6aff5c1bc5bcbe40a555f9bafd6e721c6c535f97e46c09c6700'
